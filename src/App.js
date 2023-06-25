6b42fdb816d2b9670d4c15d3730a08ea63cc99de
BarakOren <barakoren5@gmail.com>
2023-06-25 21:32:07 +0300

    update

--- src/App.js
+++ src/App.js
@@ -7,22 +7,27 @@ import Todo2 from "./components/Todo2";
 
 function App() {
 
+  //List State
   const [todoList, setTodoList] = useState([])
 
+  //Input State
   const [input, setInput] = useState("");
 
-  // when input changes
+  // Function for input changes
   const handleChange = (event) => {
       event.preventDefault();
       setInput(event.target.value);
   }
 
+
+  // function for adding a new todo
   const Add = () => {
       const object = {
           todo: input,
           id: uuidv4()
       }
-
+      //אל תשכחי שיצרנו אובייקט כדי להכיל בו את המידע שלנו בצורה נוחה
+      //וישר אחרי זה, פשוט דחפנו את האובייקט הזה לרשימה שלנו
       setTodoList([...todoList, object])
 
   }
@@ -30,42 +35,39 @@ function App() {
 
   //remove from list
   const remove = (id) => {
+    // אני רוצה שתשימי לב ממש טוב מאיפה מגיע הארגיומט שנקרא id
+    // בסופו של, הוא מגיע מאיפה שקוראים לפונקציה 
+    // שזה נמצא ב Todo2.js
       const newArray = todoList.filter(todo => todo.id !== id)
       setTodoList(newArray)
   }
 
   return (
     <div className='App'>
-    <h1>To Do App</h1>
-
-    <input 
-    type="text"
-    value={input}
-    onChange={handleChange}
-    />
-
-    <button onClick={Add}>Add</button>     
-    <button onClick={() => {setTodoList([])}}>Delete All</button>
-    
-
-    <div className="todoList"> 
-    {todoList.map((task, index) => {
-        return <Todo2 
-        number={index + 1}
-        key={task.id}
-        todo={task.todo}
-        id={task.id}
-        remove={remove}
-        todoList={todoList} 
-        setTodoList={setTodoList}
+        <h1>To Do App</h1>
+        <input 
+        type="text"
+        value={input}
+        onChange={handleChange}
         />
-    } )}
-    
 
-    </div>
+        <button onClick={Add}>Add</button>     
+        <button onClick={() => {setTodoList([])}}>Delete All</button>    
 
+        <div className="todoList"> 
+        {todoList.map((task, index) => {
+            return <Todo2 
+            number={index + 1}
+            key={task.id}
+            todo={task.todo}
+            id={task.id}
+            remove={remove}
+            todoList={todoList} 
+            setTodoList={setTodoList}
+            />
+        } )}
+        </div>
 
-    
     </div>
   );
 }
@@ -84,31 +86,3 @@ export default App;
 
 
 
-
-// {todoList.map((todo) => {
-//   return <Todo 
-//   key={todo.id}
-//   id={todo.id}
-//   todo={todo.todo}
-//   remove={remove}
-//   todoList={todoList}
-//   setTodoList={setTodoList}
-//   />                 
-// })}
-
-// add to list
-  // const Add = () => { 
-
-  //     const obj = {
-  //       todo: input,
-  //       id: uuidv4()
-  //     }
-
-  //     setTodoList(prevArray => [...prevArray, obj])
-  //     setInput("");  
-  // }
-
-   // const remove = (id) => {
-  //     const newArray = todoList.filter(todo => todo.id !== id)  
-  //     setTodoList(newArray)
-  // }
--- src/components/Todo2.js
+++ src/components/Todo2.js
@@ -5,11 +5,13 @@ const Todo2 = ({remove, todo, number, id, todoList, setTodoList }) => {
     const [editInput, setEditInput] = useState("")
     const [toggle, setToggle] = useState(false);
 
+    //function for editing input change
     const handleEditChange = (event) => {
         event.preventDefault();
         setEditInput(event.target.value)
     }
 
+    //function for updating the todo
     const handleUpdate = () => {
         let newList = todoList.map(todo => {
             if(todo.id === id){
@@ -18,30 +20,31 @@ const Todo2 = ({remove, todo, number, id, todoList, setTodoList }) => {
             return todo;
         })
 
-
-        // לעדכן את הרשימה
         setTodoList(newList)
         setToggle(false)
         setEditInput("")
     }
 
+// יותר למטה את תראי &&
+// זה בעצם אומר תמשיך את הקוד שרשמת, רק במקרא של true
+// אם לא true מבחינתו הוא לא עושה כלום
+
     return <div className="todo">
-    <p>{number}</p>
-    <p>{todo}</p>
-    <button onClick={() => remove(id)}>Remove</button>
-    <button onClick={() => setToggle(!toggle)}>Edit</button>
+        <p>{number}</p>
+        <p>{todo}</p>
+        <button onClick={() => remove(id)}>Remove</button>
+        <button onClick={() => setToggle(!toggle)}>Edit</button>
 
-    {toggle && <div> 
-    <input 
-    type="text"
-    onChange={handleEditChange}
-    value={editInput} />
-        
-    <button onClick={() => handleUpdate()}>Update</button>
-    </div>
-    }
 
-    
+        {toggle && <div> 
+        <input 
+        type="text"
+        onChange={handleEditChange}
+        value={editInput} />
+        <button onClick={() => handleUpdate()}>Update</button>
+        </div>
+        }
+
     </div>
 }
 
